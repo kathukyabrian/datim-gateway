bbef942b54e7ed011ee2dd51678c09176ebc5ba2
brian <briankathukya2000@gmail.com>
2022-02-05 14:35:29 +0300

    Made HTTP UTIL async

--- src/main/java/hackathon/group4/api/util/HttpUtil.java
+++ src/main/java/hackathon/group4/api/util/HttpUtil.java
@@ -2,6 +2,7 @@ package hackathon.group4.api.util;
 
 import lombok.extern.log4j.Log4j2;
 import okhttp3.*;
+import org.springframework.scheduling.annotation.Async;
 
 import java.io.IOException;
 import java.util.Map;
@@ -9,6 +10,7 @@ import java.util.Map;
 @Log4j2
 public class HttpUtil {
 
+    @Async
     public static String post(String url, String body,  MediaType mediaType) throws IOException {
         log.debug("Util has received the request");
         OkHttpClient client = new OkHttpClient();
